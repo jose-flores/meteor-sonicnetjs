43a8926fa40c0d26f66308cc5d8e0595f7b7d13d
Jose Flores <jose@chefreports.com>
2015-10-10 20:00:33 -0400

    Upload real code :)

---
+++ package.js
@@ -0,0 +1,30 @@
+Package.describe({
+  name: 'joseflores:sonicnetjs',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'This package is a port of the sonicnetjs from https://github.com/borismus/sonicnet.js. So all credits to Boris Smus (https://github.com/borismus)',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.2');
+  api.use('ecmascript');
+  api.addFiles(['sonic-coder.js','ring-buffer.js','sonic-socket.js','sonic-server.js'],'client');
+  if (api.export){
+    api.export('SonicCoder');
+    api.export('RingBuffer');
+    api.export('SonicServer');
+    api.export('SonicSocket');
+  }
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('joseflores:soniccoder');
+  api.addFiles('soniccoder-tests.js');
+});
